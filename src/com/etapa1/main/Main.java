dab31b23ffb0208af125623c39d45f2edd82d319
nourjandali <nourmshjan@gmail.com>
2019-11-19 23:57:17 +0200

    Initial commit

---
+++ src/com/etapa1/main/Main.java
@@ -0,0 +1,11 @@
+package com.etapa1.main;
+
+public final class Main {
+  private Main() {
+    // just to trick checkstyle
+  }
+
+  public static void main(final String[] args) {
+    System.out.println("Hello World");
+  }
+}
