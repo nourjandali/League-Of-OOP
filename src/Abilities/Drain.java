4215e9fb9226930d12115317b1bc18e34af86825
Nour JANDALI (100009) <nour.jandali@stud.fils.upb.ro>
2019-12-01 17:30:04 +0200

    Finish Drain

---
+++ src/Abilities/Drain.java
@@ -0,0 +1,23 @@
+package Abilities;
+
+import Heroes.Hero;
+import Heroes.HeroesType;
+
+public class Drain extends Ability {
+  double percentage;
+
+  public Drain(int level) {
+    this.percentage = 0.2 + (level * 0.05);
+    modifiers.put(HeroesType.Rogue, -0.2);
+    modifiers.put(HeroesType.Knight, 0.2);
+    modifiers.put(HeroesType.Pyromancer, -0.1);
+    modifiers.put(HeroesType.Wizard, 0.05);
+  }
+
+  @Override
+  public double execute(Hero enemyHero) {
+    double basicHP = Math.min(0.3*enemyHero.getInitHP(), enemyHero.getHP());
+    double totalPercentage = this.percentage + (this.percentage*modifiers.get(enemyHero.getType()));
+    return (basicHP*totalPercentage);
+  }
+}
--- src/Heroes/Wizard.java
+++ src/Heroes/Wizard.java
@@ -1,4 +1,39 @@
 package Heroes;
 
-public class Wizard {
+import Abilities.AbilitiesFactory;
+
+import java.util.ArrayList;
+
+public class Wizard extends Hero {
+  public Wizard(ArrayList<Integer> position) {
+    super(position);
+    this.initHP = 400;
+    this.HP = this.initHP;
+    type = HeroesType.Wizard;
+  }
+
+  @Override
+  public void levelUp() {
+    if (this.XP > getThreshold()) {
+      this.level++;
+      this.XP -= getThreshold();
+      // Resetting HP
+      initHP += 30;
+      this.HP = this.initHP;
+    }
+  }
+
+  @Override
+  public double getTotalDamage(Hero enemyHero, char terrainType, int round) {
+      AbilitiesFactory abilitiesFactory = AbilitiesFactory.getInstance();
+      double drainDamage =
+              abilitiesFactory.createAbility("Drain", this.level, round).execute(enemyHero);
+      double deflectDamage =
+              abilitiesFactory.createAbility("Deflect", this.level, round).execute(enemyHero);
+      double totalDamage = drainDamage + deflectDamage;
+      if (terrainType == 'D') {
+          totalDamage *= 1.10;
+      }
+      return totalDamage;
+  }
 }
